f94b7567f32850a27b3d5490ae3a06f51e8b04ea
Marlon Leoner <marlon.leoner@gmail.com>
2025-06-23 19:17:51 -0300

    spring project initial commit

---
+++ src/main/java/marlon/leoner/technical/assessment/Application.java
@@ -0,0 +1,13 @@
+package marlon.leoner.technical.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/marlon/leoner/technical/assessment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package marlon.leoner.technical.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
